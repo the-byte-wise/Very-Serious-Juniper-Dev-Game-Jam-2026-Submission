0618dff8aeaf6c799d4e304595e05018793782ee
Byte Wise <accounts@thebytewise.xyz>
2026-06-22 15:26:18 -0500

    Ball velocity and collision with paddle

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,15 +3,45 @@ extends CharacterBody2D
 const ROTATION_SPEED = 3.0
 const RADIUS = 600.0
 const CENTER = Vector2.ZERO
+const BOUNCE_FORCE = 600.0
 
 var angle: float = PI
 
-func _ready():
+func _ready() -> void:
 	global_position = CENTER + Vector2(cos(angle), sin(angle)) * RADIUS
 
+# Not good for collision physics,
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_axis("rotate_c_clockwise", "rotate_clockwise")
 	
 	angle += direction * ROTATION_SPEED * delta
 	global_position = CENTER + Vector2(cos(angle), sin(angle)) * RADIUS
+	
 	look_at(CENTER)
+	
+func _on_ball_entered(body: Node):
+	var ball := body as RigidBody2D
+	
+	if not ball:
+		return
+	
+	var direction = (ball.global_position - global_position).normalized()
+	ball.apply_central_impulse(direction * BOUNCE_FORCE)
+
+# Not good for keeping the paddle on the circle but does the physics math automatically	
+#func _physics_process(delta: float) -> void:
+	#var offset = global_position - CENTER
+	#var distance = offset.length()
+	#
+	#var input = Input.get_axis("rotate_c_clockwise", "rotate_clockwise")
+	#
+	#var tangent = Vector2(-offset.y, offset.x).normalized()
+	#velocity = tangent * input * ROTATION_SPEED * RADIUS
+	#
+	## Readjust if a collision pushes the paddle away from the circle
+	#if distance > 0:
+		#var radial_dir = offset / distance
+		#velocity += radial_dir * (distance - RADIUS) * -10.0 * delta
+	#
+	#move_and_slide()
+	#look_at(CENTER)
